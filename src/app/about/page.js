19f03d13b6bb7679c6e9ef83a2da3bd89ea61458
amitsengupta <amitsengupta332@gmail.com>
2025-09-16 23:48:03 +0600

    first

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+ 
+
+const AboutPage = () => {
+  return <div> 
+    <h1>about page</h1>
+    <p>This is the about page</p>
+  </div>;
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div> 
+      <h1 className='text-3xl text-center underline'>contact page</h1>
+      <p>This is the contact page</p>
+    </div>
+  )
+}
+
+export default ContactPage
