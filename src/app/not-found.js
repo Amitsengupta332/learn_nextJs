92e608b50acfa2fee51aa0e1bc185499f79cbf61
amitsengupta <amitsengupta332@gmail.com>
2025-09-17 15:07:54 +0600

    link the route and nextjs built in route

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div> 
+        <h1 className='text-3xl text-center underline'>404 not found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
